39ff41563aaf4c5e5ac8977e77c5b0f3cbe68b3d
Tommy Johnson <tommyjohnson1028@gmail.com>
2022-01-27 14:45:00 -0800

    add list command

--- scripts/transfer.ts
+++ scripts/transfer.ts
@@ -15,6 +15,12 @@ import {
   u64,
 } from "@solana/spl-token";
 import { programs } from "@metaplex/js";
+import {
+  Edition,
+  EditionMarker,
+  MasterEdition,
+} from "@metaplex-foundation/mpl-token-metadata";
+import BN from "bn.js";
 
 const METADATA_PROGRAM_ID = new PublicKey(
   "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
@@ -37,6 +43,95 @@ function programCommand(name: string) {
       "--keypair not provided"
     );
 }
+programCommand("list")
+  .description("List NFTs in a wallet")
+  .option("-s, --symbol <string>", "Symbol to filter NFTs on", "CC")
+  .option(
+    "-r, --rpc-url <string>",
+    "custom rpc url since this is a heavy command"
+  )
+  .action(async (options, cmd) => {
+    const { keypair, env, symbol, rpcUrl: rpcUrlArg } = cmd.opts();
+
+    // Load wallet keypair
+    const payer = Keypair.fromSecretKey(
+      Buffer.from(
+        JSON.parse(
+          require("fs").readFileSync(keypair, {
+            encoding: "utf-8",
+          })
+        )
+      )
+    );
+
+    // Create connection to RPC cluster
+    const rpcUrl = rpcUrlArg || clusterApiUrl(env);
+    const connection = new Connection(rpcUrl);
+
+    // Get all tokens from the wallet
+    console.log(`Getting all tokens for wallet: ${payer.publicKey}`);
+    const resp = await connection.getTokenAccountsByOwner(payer.publicKey, {
+      programId: TOKEN_PROGRAM_ID,
+    });
+    const tokenAccounts = await Promise.all(
+      resp.value.map(async ({ account, pubkey }, index) => {
+        // Avoid RPC rate limites
+        await wait(index * 2_500);
+
+        const decoded = AccountLayout.decode(account.data);
+        const tokenAccount = {
+          address: pubkey,
+          mint: new PublicKey(decoded.mint),
+          owner: new PublicKey(decoded.owner),
+          amount: new u64(decoded.amount),
+          delegate: new PublicKey(decoded.delegate),
+          isInitialized: decoded.state === 1,
+          isNative: decoded.isNativeOption === 1,
+          delegatedAmount: new u64(decoded.delegatedAmount),
+          closeAuthority: new PublicKey(decoded.closeAuthority),
+        };
+        // Look up Metadata account with memcmp filter based on the token mint
+        console.log(`Looking up program accounts for index: ${index}`);
+        const res = await connection.getProgramAccounts(METADATA_PROGRAM_ID, {
+          filters: [
+            {
+              memcmp: {
+                offset:
+                  1 + // key
+                  32, // update auth
+                // 32 + // mint
+                bytes: tokenAccount.mint.toBase58(),
+              },
+            },
+          ],
+        });
+        // If there is a Metadata account, load and deserialize the data
+        const metadataAccount = res[0];
+        let metaData = undefined;
+        if (metadataAccount) {
+          metaData = await programs.metadata.Metadata.load(
+            connection,
+            metadataAccount.pubkey
+          );
+        }
+        return { tokenAccount, metaData };
+      })
+    );
+
+    console.log("Filtering token accounts for metadata (i.e. only NFTs)");
+    let nftAccounts = tokenAccounts.filter(
+      (x) => x.metaData && x.metaData.data
+    );
+    if (symbol) {
+      console.log(`Filtering accounts by metadta symbol: ${symbol}`);
+      // Filter all owned NFTs by symbol
+      nftAccounts = tokenAccounts.filter(
+        (x) => x.metaData && x.metaData.data.data.symbol === symbol
+      );
+    }
+    console.log('NFT mint addresses\n')
+    nftAccounts.forEach((account) => console.log(account.tokenAccount.mint.toString()))
+  });
 
 programCommand("transfer")
   .argument(
@@ -46,8 +141,8 @@ programCommand("transfer")
   )
   .option("-s, --symbol <string>", "Symbol to filter NFTs for transfer", "CC")
   .option(
-    '-r, --rpc-url <string>',
-    'custom rpc url since this is a heavy command',
+    "-r, --rpc-url <string>",
+    "custom rpc url since this is a heavy command"
   )
   .action(async (toKey, options, cmd) => {
     const { keypair, env, symbol, rpcUrl: rpcUrlArg } = cmd.opts();
@@ -117,10 +212,17 @@ programCommand("transfer")
       })
     );
 
-    // Filter all owned NFTs by symbol
-    const nftAccounts = tokenAccounts.filter(
-      (x) => x.metaData && x.metaData.data.data.symbol === symbol
+    console.log("Filtering token accounts for metadata (i.e. only NFTs)");
+    let nftAccounts = tokenAccounts.filter(
+      (x) => x.metaData && x.metaData.data
     );
+    if (symbol) {
+      console.log(`Filtering accounts by metadta symbol: ${symbol}`);
+      // Filter all owned NFTs by symbol
+      nftAccounts = tokenAccounts.filter(
+        (x) => x.metaData && x.metaData.data.data.symbol === symbol
+      );
+    }
 
     // Initiate the transfer for each NFT
     console.log(
@@ -183,12 +285,54 @@ programCommand("transfer")
         await sendAndConfirmTransaction(connection, transaction, [payer], {
           commitment: "confirmed",
         });
-      } catch(error) {
-        console.log("Error sending item: ", error)
+      } catch (error) {
+        console.log("Error sending item: ", error);
       }
-      
+
       return null;
     }, starterPromise);
+  });
+
+program
+  .command("getEditionsFromMaster")
+  .description("WORK IN PROGRESS DO NOT USE IN PRODUCTION")
+  .option(
+    "-e, --env <string>",
+    "Solana cluster env name",
+    "devnet" //mainnet-beta, testnet, devnet
+  )
+  .argument(
+    "<masterEditionAddress>",
+    "The address of the master edition that minted the Editions.",
+    (val) => new PublicKey(val)
+  )
+  .option(
+    "-r, --rpc-url <string>",
+    "custom rpc url since this is a heavy command"
+  )
+  .action(async (masterEditionKey, options, cmd) => {
+    const { env, rpcUrl: rpcUrlArg } = cmd.opts();
+    const rpcUrl = rpcUrlArg || clusterApiUrl(env);
+    const connection = new Connection(rpcUrl);
+    const masterEditionPda = await MasterEdition.getPDA(masterEditionKey);
+
+    // Load the master edition account info
+    const masterEditionAccountInfo = await connection.getAccountInfo(
+      masterEditionPda,
+      "processed"
+    );
+    if (!masterEditionAccountInfo) {
+      throw new Error(`MasterEdition not found for ${masterEditionKey}`);
+    }
+
+    // Create a MasterEdition instance
+    const masterEdition = new MasterEdition(
+      masterEditionPda,
+      masterEditionAccountInfo
+    );
+    // TODO: load all of the editions
+    const editions = await masterEdition.getEditions(connection);
+    console.log("*** editions\n", editions);
   });
 
 program.parse(process.argv);
